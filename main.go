f115b41eeeb1eff5431fdcd638e3a7ec3f3c2404
Balint Mucsanyi <balint.mucsanyi@nokia.com>
2026-01-25 14:04:40 +0100

    added initial struct for http methods, owner of timestamp func

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type operation int
+
+const (
+	opSet operation = iota
+	opGet
+)
+
+type request struct {
+	op      operation
+	value   time.Time
+	replyCh chan time.Time
+}
+
+func timestampOwner(reqCh <-chan request) {
+	var stored time.Time
+	for req := range reqCh {
+		switch req.op {
+		case opSet:
+			stored = req.value
+		case opGet:
+			req.replyCh <- stored
+		}
+	}
+}
 
 func main() {
 	fmt.Println("Hello, World!")
+	reqCh := make(chan request)
+	go timestampOwner(reqCh)
+
+	select {}
 }
