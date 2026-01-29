d503a7a8deac1c8c0a478d8024df2b6bc92e5749
Balint Mucsanyi <balint.mucsanyi@nokia.com>
2026-01-29 13:45:06 +0100

    also support clientside

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -91,12 +92,66 @@ func handleGetTimestamp(reqCh chan<- request) http.HandlerFunc {
 	}
 }
 
+func startServer(reqCh chan request) {
+	http.HandleFunc("/timestamp", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			handlePostTimestamp(reqCh)(w, r)
+			return
+		}
+		if r.Method == http.MethodGet {
+			handleGetTimestamp(reqCh)(w, r)
+			return
+		}
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+	})
+
+	// no logging to stdout
+	_ = http.ListenAndServe(":8080", nil)
+}
+
+func runClient() (int64, error) {
+	client := &http.Client{}
+
+	ts := time.Now().Unix()
+	body := strconv.FormatInt(ts, 10)
+
+	// POST timestamp
+	resp, err := client.Post(
+		"http://localhost:8080/timestamp",
+		"text/plain",
+		strings.NewReader(body),
+	)
+	if err != nil {
+		return 0, err
+	}
+	resp.Body.Close()
+
+	// GET timestamp
+	resp, err = client.Get("http://localhost:8080/timestamp")
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return 0, err
+	}
+
+	readTs, err := strconv.ParseInt(string(respBody), 10, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return readTs, nil
+}
+
+// For main_test.go usage
 func newServer() http.Handler {
 	reqCh := make(chan request)
 	go timestampOwner(reqCh)
 
 	mux := http.NewServeMux()
-
 	mux.HandleFunc("/timestamp", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodPost {
 			handlePostTimestamp(reqCh)(w, r)
@@ -114,7 +169,18 @@ func newServer() http.Handler {
 
 func main() {
 
-	fmt.Println("listening on :8080")
-	http.ListenAndServe(":8080", newServer())
+	reqCh := make(chan request)
+	go timestampOwner(reqCh)
 
+	go startServer(reqCh)
+
+	// small delay to ensure server is ready
+	time.Sleep(100 * time.Millisecond)
+
+	ts, err := runClient()
+	if err != nil {
+		return
+	}
+
+	fmt.Print(ts)
 }
